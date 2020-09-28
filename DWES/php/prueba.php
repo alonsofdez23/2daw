41e0df743b384785a1fced838b92ddca5bf6ce6e
Alonso Fernández Vidal <alonsobs23@gmail.com>
2020-09-28 19:34:08 +0200

    DWES. Primer archivo php

---
+++ DWES/php/prueba.php
@@ -0,0 +1,10 @@
+<?php
+echo "Hola, esta es la primera línea\n";
+echo "Esta es la segunda\n";
+echo "El resultado de la suma es ", 2 + 3, "\n";
+?>
+kjhfhdsklksdfgfjksdbgbfsdljgbkljsdfbng
+<?php
+echo 2 + 3, "\n";
+?>
+fin
