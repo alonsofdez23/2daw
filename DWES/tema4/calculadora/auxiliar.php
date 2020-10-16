22566433b50b4fd4d764d437a6d3bba1dcf0d764
Alonso Fernández Vidal <alonsobs23@gmail.com>
2020-10-16 19:33:15 +0200

    DWES. Insertar todo en index.php en holamundo. Comenzada calculadora

---
+++ DWES/tema4/calculadora/auxiliar.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * error
+ *
+ * @param  string $mensaje
+ * @return bool  
+ */
+function error($mensaje)
+{
+    echo "<h3>Error: $mensaje</h3>";
+    return true;
+}
+
+/**
+ * Calcula el resultado de realizar la opración indicada
+ * en $op sobre los operandos $op1 y $op2
+ *
+ * @param  mixed $op1
+ * @param  mixed $op2
+ * @param  mixed $op
+ * @return mixed El resultado de la operación
+ */
+function calcular($op1, $op2, $op)
+{
+    switch ($op) {
+        case '+':
+            $res = $op1 + $op2;
+            break;
+        case '-':
+            $res = $op1 - $op2;
+            break;
+        case '*':
+            $res = $op1 * $op2;
+            break;
+        case '/':
+            $res = $op1 / $op2;
+            break;
+    }
+    return $res;
+}
--- DWES/tema4/holamundo/index.php
+++ DWES/tema4/holamundo/index.php
@@ -8,14 +8,35 @@
 </head>
 
 <body>
+    <?php if (isset($_GET['nombre'])): ?>
+        <?php $nombre = trim($_GET['nombre']) ?>
+        <?php if ($nombre != ''): ?>
+            <h3>¡Hola, <?= $nombre ?>!</h3>
+        <?php else: ?>
+            <h2>Error: el nombre no puede ser vacío.</h2>
+        <?php endif ?>
+    <?php endif ?>
+    <?php if (isset($_GET['anyo'])): ?>
+        <?php $anyo = trim($_GET['anyo']) ?>
+        <?php if ($anyo != ''): ?>
+            <?php if (ctype_digit($anyo) && $anyo <= 9999): ?>
+                <p>Naciste en <?= $anyo ?> y tienes <?= date('Y') - $anyo ?> años.</p>
+            <?php else: ?>
+                <h2>Error: el año no tiene el formato adecuado.</h2>
+            <?php endif ?>
+        <?php endif ?>
+    <?php endif ?>
+
     <!-- <?php echo "Hola, mundo" ?><br>
     <?= "Hola, mundo" ?><br>
     <?= php_sapi_name() ?><br>
     <?= phpinfo() ?> -->
 
-    <form action="saluda.php" method="get">
-        <label for="">Escribe tu nombre</label>
+    <form action="index.php" method="get">
+        <label for="">Escribe tu nombre:*</label>
         <input type="text" name="nombre" id="nom">
+        <label for="">Escribe tu año de nacimiento:</label>
+        <input type="text" name="anyo" id="anyo" size="10">
         <button type="submit">Saludar</button>
     </form>
 
