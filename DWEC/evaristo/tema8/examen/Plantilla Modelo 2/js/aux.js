87ca66c675874fe164f4d357013583ee3c9956d4
alonsofdez23 <alonso.fernandez@iesdonana.org>
2021-03-02 19:23:21 +0100

    DWEC. Examen jQuery final

--- DWEC/evaristo/tema8/examen/Plantilla Modelo 2/js/aux.js
+++ DWEC/evaristo/tema8/examen/Plantilla Modelo 2/js/aux.js
@@ -1,7 +1,42 @@
 $(function(){
+    function crearTd(contenido, contenedor) {
+        let td = $("<td>").text(contenido)
+        contenedor.append(td)
+    }
+
     function solicitarInformacion() {
         $.ajax({
-            
+            type: "GET",
+            url: "https://jsonplaceholder.typicode.com/users",
+            dataType: "json"
         })
+            .done(mostrarInformacion)
     }
+
+    function mostrarInformacion(respuestaJson) {
+        console.log(respuestaJson);
+        let usuarios = respuestaJson;
+
+        let tabla = $("table")
+
+        for (usuario of usuarios) {
+
+            let id = usuario.id;
+            let email = usuario.email;
+            let ciudad = usuario.address;
+            
+            let fila = $("<tr>")
+
+            crearTd(id, fila);
+            crearTd(email, fila);
+            crearTd(ciudad, fila);
+            
+            tabla.append(fila)
+        }
+
+        tabla.fadeIn("fast")
+    }
+
+    $("table").hide()
+    solicitarInformacion();
 })
